510910c7aa4ad7b933beadde72b7f6b7d6e01683
yuzhang-npu <2217943649@qq.com>
2021-09-09 20:53:19 +0800

    修复中文乱码的问题

--- serverlib/example/MyblogServer/blogserver_test.cpp
+++ serverlib/example/MyblogServer/blogserver_test.cpp
@@ -1,7 +1,6 @@
 //
 // Created by echo-djx on 2021/9/2.
 //
-
 #include <HttpConnection/HttpServer.h>
 #include <HttpConnection/HttpRequest.h>
 #include <HttpConnection/HttpResponse.h>
@@ -11,89 +10,78 @@
 #include <TcpConnection/Buffer.h>
 #include <TcpConnection/Timestamp.h>
 
+#include <sstream>
+#include <fstream>
+#include <codecvt>
 #include <iostream>
 #include <map>
+#include <locale>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string>
-#include <fstream>
 #include <sys/stat.h>
-#include <sstream>
+#include <locale.h>
+
 bool benchmark = false;
 
 void onRequest(const HttpRequest &req, HttpResponse *resp) {
     std::cout << "Headers " << req.methodString() << " " << req.path() << std::endl;
     if (!benchmark) {
-        const std::map<std::string, std::string> &headers = req.headers();
+        const std::map <std::string, std::string> &headers = req.headers();
         for (const auto &header : headers) {
             std::cout << header.first << ": " << header.second << std::endl;
         }
     }
     std::string filename = req.path();
-    if( filename == "/" || filename == "/index.html" ) {
-            resp->setContentType("text/html");
-    } else if( filename.find( ".jpg" ) != std::string::npos || filename.find( ".png" ) != std::string::npos ) {
-            resp->setContentType("image/jpg");
-    } else if( filename.find( ".html" ) != std::string::npos ) {
-            resp->setContentType("text/html");
-    } else if( filename.find( ".ico") != std::string::npos ) {
-            resp->setContentType("image/x-icon");
-    } else if( filename.find( ".js") != std::string::npos ) {
-            resp->setContentType("yexy/javascript");
-    } else if( filename.find( ".css" ) != std::string::npos ) {
-            resp->setContentType("text/css");
-    } else if( filename.find( ".mp3" ) != std::string::npos ) {
-            resp->setContentType("audio/mp3");
-    } else if( filename.find( ".mp4" ) != std::string::npos ) {
-            resp->setContentType("audio/mp4");
-        } else {
-            resp->setContentType("text/plain");
-        }
+    if (filename == "/" || filename == "/index.html") {
+        resp->setContentType("text/html");
+    } else if (filename.find(".jpg") != std::string::npos || filename.find(".png") != std::string::npos) {
+        resp->setContentType("image/jpg");
+    } else if (filename.find(".html") != std::string::npos) {
+        resp->setContentType("text/html;charset=utf-8");
+    } else if (filename.find(".ico") != std::string::npos) {
+        resp->setContentType("image/x-icon");
+    } else if (filename.find(".js") != std::string::npos) {
+        resp->setContentType("yexy/javascript");
+    } else if (filename.find(".css") != std::string::npos) {
+        resp->setContentType("text/css");
+    } else if (filename.find(".mp3") != std::string::npos) {
+        resp->setContentType("audio/mp3");
+    } else if (filename.find(".mp4") != std::string::npos) {
+        resp->setContentType("audio/mp4");
+    } else {
+        resp->setContentType("text/plain");
+    }
     filename = req.path().substr(1);
-    if(filename == "index" || filename == "")
-    {
-        filename  = "index.html";
+    if (filename == "index" || filename == "") {
+        filename = "index.html";
+        resp->setContentType("text/html");
+    }
+    if (filename.find(".") == std::string::npos) {
+        filename = filename + "/index.html";
+        resp->setContentType("text/html");
     }
     struct stat filestat;
-    int ret = stat( filename.c_str(), &filestat );
-    if( ret < 0 || !S_ISREG( filestat.st_mode ) )
-    {  // 打开文件出错或没有该文件
-        LOG_ERROR("can't find:%s \n",filename.c_str());
+    int ret = stat(filename.c_str(), &filestat);
+    if (ret < 0 || !S_ISREG(filestat.st_mode)) {
         resp->setStatusCode(HttpResponse::k404NotFound);
         resp->setStatusMessage("Not Found");
         resp->setCloseConnection(true);
-    } else
-    {
-        LOG_ERROR("尝试读取文件:%s \n",filename.c_str());
+    } else {
         resp->setStatusCode(HttpResponse::k200Ok);
         resp->setStatusMessage("OK");
         std::string tmp;
         std::string buf;
-        std::ifstream  fin(filename.c_str());
-        fin.(std::locale("en_US.UTF-8"));
-        while(std::getline(fin,tmp))
-        {
+        std::ifstream fin(filename.c_str());
+        // fin.imbue(std::locale("en_US.UTF-8"));
+        while (std::getline(fin, tmp)) {
             buf = buf + tmp;
             buf.append("\n");
         }
         fin.close();
         resp->setBody(buf);
     }
-//    if (req.path() == "/") {
-//        resp->setStatusCode(HttpResponse::k200Ok);
-//        resp->setStatusMessage("OK");
-//        resp->setContentType("text/html");
-//        resp->addHeader("Server", "djx");
-//        std::string now = Timestamp::now().toString();
-//        resp->setBody("<html><head><title>This is title</title></head>"
-//                      "<body><h1>Hello</h1>Now is " + now +
-//                      "</body></html>");
-//    } else if (req.path() == "/favicon.ico") {
-//        resp->setStatusCode(HttpResponse::k200Ok);
-//        resp->setStatusMessage("OK");
-//        resp->setContentType("image/png");
-//        resp->setBody(std::string(favicon, sizeof favicon));
-//    }
+
 }
 
 
